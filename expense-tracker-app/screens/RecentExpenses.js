6b5ccd5a013ea8e982cc39ede73d5de294cdb53e
ioana.girba <>
2024-04-03 18:30:14 +0300

    Add loader

--- expense-tracker-app/screens/RecentExpenses.js
+++ expense-tracker-app/screens/RecentExpenses.js
@@ -1,25 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       expensesCtx.setExpenses(expenses);
     }
+
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-    // console.log(recentExpenses);
+
     return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
