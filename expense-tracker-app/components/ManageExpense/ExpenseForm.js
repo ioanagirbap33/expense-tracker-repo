f19aa5257173e17b895fcf2ce67d4894810d0bb1
ioana.girba <>
2024-04-03 15:12:05 +0300

    Make form functional

--- expense-tracker-app/components/ManageExpense/ExpenseForm.js
+++ expense-tracker-app/components/ManageExpense/ExpenseForm.js
@@ -1,12 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
-import { Input } from "./Input";
 import { useState } from "react";
 
-export const ExpenseForm = () => {
+import { Input } from "./Input";
+import Button from "../UI/Button";
+
+export const ExpenseForm = ({
+  submitButtonLabel,
+  onCancel,
+  onSubmit,
+  defaultValues,
+}) => {
   const [inputValues, setInputValues] = useState({
-    amount: "",
-    date: "",
-    description: "",
+    amount: defaultValues ? defaultValues.amount.toString() : "",
+    date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : "",
+    description: defaultValues ? defaultValues.description.toString() : "",
   });
 
   const inputChangedHandler = (inputIdentifier, enteredAmount) => {
@@ -17,6 +24,16 @@ export const ExpenseForm = () => {
       };
     });
   };
+
+  const submitHandler = () => {
+    const expenseData = {
+      amount: +inputValues.amount,
+      date: new Date(inputValues.date),
+      description: inputValues.description,
+    };
+    onSubmit(expenseData);
+  };
+
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Expenses</Text>
@@ -51,6 +68,14 @@ export const ExpenseForm = () => {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 };
@@ -72,5 +97,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
